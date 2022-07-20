10a83080e87cf7fa3e218a71bbcea8f3542488fe
KeerthanaM15 <keerthanahari456@gmail.com>
2022-07-20 16:04:16 +0000

    updated design

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -2,6 +2,7 @@
 
 import cocotb
 from cocotb.triggers import Timer
+import random
 
 @cocotb.test()
 async def test_mux(dut):
@@ -46,7 +47,7 @@ async def test_mux(dut):
     I30 = 1
     I31 = 0
 
-    SEL=14
+    SEL=12
 
     dut.inp0.value = I0
     dut.inp1.value = I1
@@ -86,12 +87,106 @@ async def test_mux(dut):
     dut.inp28.value = I28
     dut.inp29.value = I29
     dut.inp30.value = I30
-    dut.inp31.value = I31
+    #dut.inp31.value = I31
 
     dut.sel.value = SEL
 
     await Timer(2,units='ns')
-    dut._log.info(f'SEL={SEL:05} INPUT={I14:05} model={I14:05} DUT={int(dut.out.value):05}')
-    assert dut.out.value == I14, "selected value is incorrect: {SEL} {I14} != {out}, expected value = {EXP}".format(SEL=int(dut.sel.value), I14=int(dut.inp14.value), out=int(dut.out.value), EXP=I14)
+    dut._log.info(f'SEL={SEL:05} INPUT={I12:05} model={I12:05} DUT={int(dut.out.value):05}')
+    assert dut.out.value == I12, "selected value is incorrect: {SEL} {I12} != {out}, expected value = {EXP}".format(SEL=int(dut.sel.value), I12=int(dut.inp12.value), out=int(dut.out.value), EXP=I12)
     
-    cocotb.log.info('##### CTB: Develop your test here ########')
+@cocotb.test()
+async def mux_randomised_test(dut):
+    I=[None for i in range(32)]
+    for i in range(16):
+        print("*****TEST_MUX******")
+        I[0] = 0
+        I[1] = 1
+        I[2] = 2
+        I[3] = 3
+
+        I[4] = 1
+        I[5] = 2
+        I[6] = 3
+        I[7] = 0
+
+        I[8] = 2
+        I[9] = 3
+        I[10] = 0
+        I[11] = 1
+
+        I[12] = 3
+        I[13] = 0
+        I[14] = 1
+        I[15] = 2
+
+        I[16] = 3
+        I[17] = 2
+        I[18] = 1
+        I[19] = 0
+
+        I[20] = 2
+        I[21] = 1
+        I[22] = 0
+        I[23] = 3
+
+        I[24] = 1
+        I[25] = 0
+        I[26] = 3
+        I[27] = 2
+
+        I[28] = 0
+        I[29] = 3
+        I[30] = 2
+        I[31] = 1
+
+        SEL = random.randint(0,31)
+        temp = SEL
+
+        dut.inp0.value = I[0]
+        dut.inp1.value = I[1]
+        dut.inp2.value = I[2]
+        dut.inp3.value = I[3]
+
+        dut.inp4.value = I[4]
+        dut.inp5.value = I[5]
+        dut.inp6.value = I[6]
+        dut.inp7.value = I[7]
+
+        dut.inp8.value = I[8]
+        dut.inp9.value = I[9]
+        dut.inp10.value = I[10]
+        dut.inp11.value = I[11]
+
+        dut.inp12.value = I[12]
+        dut.inp13.value = I[13]
+        dut.inp14.value = I[14]
+        dut.inp15.value = I[15]
+
+        dut.inp16.value = I[16]
+        dut.inp17.value = I[17]
+        dut.inp18.value = I[18]
+        dut.inp19.value = I[19]
+
+        dut.inp20.value = I[20]
+        dut.inp21.value = I[21]
+        dut.inp22.value = I[22]
+        dut.inp23.value = I[23]
+
+        dut.inp24.value = I[24]
+        dut.inp25.value = I[25]
+        dut.inp26.value = I[26]
+        dut.inp27.value = I[27]
+
+        dut.inp28.value = I[28]
+        dut.inp29.value = I[29]
+        dut.inp30.value = I[30]
+        #dut.inp31.value = I[31]
+
+        dut.sel.value = SEL
+
+        await Timer(2,units='ns')
+
+        dut._log.info(f'SEL={SEL:05} INPUT={I[temp]:05} EXPECTED={I[temp]:05} ACTUAL_DUT={int(dut.out.value):05}')
+
+        assert dut.out.value == I[temp], "selected value is incorrect: {SEL} {INPUT} != {OUT}, expected value = {EXP}".format(SEL=int(dut.sel.value), INPUT=I[temp], OUT=int(dut.out.value), EXP=I[temp])
